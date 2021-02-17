96b28e21f10c7b324612d728f23e94165389dc60
Farshad Shafii <farshad.shafii@gmail.com>
2021-02-17 17:25:20 +0000

    imported the language card list object and assigned it to languageCards variable

--- src/app/language-card/language-card.component.ts
+++ src/app/language-card/language-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ILanguage } from './language';
+import { LANGUAGECARDS } from './language-list';
 
 @Component({
   selector: 'app-language-card',
@@ -7,26 +8,7 @@ import { ILanguage } from './language';
   styleUrls: ['./language-card.component.css'],
 })
 export class LanguageCardComponent implements OnInit {
-  languageCards: ILanguage[] = [
-    {
-      language: 'Urdu',
-      greeting: 'ہیلو دنیا',
-      button: 'Click here to get started!',
-      available: true,
-    },
-    {
-      language: 'French',
-      greeting: 'Bonjour le monde',
-      button: 'Currently not available',
-      available: false,
-    },
-    {
-      language: 'Spanish',
-      greeting: 'Hola Mundo',
-      button: 'Currently not available',
-      available: false,
-    },
-  ];
+  languageCards: ILanguage[] = LANGUAGECARDS;
 
   selectClicked(i): void {
     console.log(this.languageCards[i].language);
