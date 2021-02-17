4703ce7f554e42a638b76816741b5920dd03032d
Farshad Shafii <farshad.shafii@gmail.com>
2021-02-17 15:48:04 +0000

    added material card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LanguageCardComponent } from './language-card/language-card.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, NavBarComponent, LanguageCardComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, MatCardModule, NoopAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
