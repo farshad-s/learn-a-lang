808f7db6596953d1d43ad9cbbd46c012d7c2dac2
Farshad Shafii <farshad.shafii@gmail.com>
2021-02-17 17:25:00 +0000

    created a new file to hold the language card list object

---
+++ src/app/language-card/language-list.ts
@@ -0,0 +1,22 @@
+import { ILanguage } from './language';
+
+export const LANGUAGECARDS: ILanguage[] = [
+  {
+    language: 'Urdu',
+    greeting: 'ہیلو دنیا',
+    button: 'Click here to get started!',
+    available: true,
+  },
+  {
+    language: 'French',
+    greeting: 'Bonjour le monde',
+    button: 'Currently not available',
+    available: false,
+  },
+  {
+    language: 'Spanish',
+    greeting: 'Hola Mundo',
+    button: 'Currently not available',
+    available: false,
+  },
+];
