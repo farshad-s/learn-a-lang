b0622521961006197c359f76e1074175c5d28941
Farshad Shafii <farshad.shafii@gmail.com>
2021-02-17 16:34:41 +0000

    created an array of objects, placing the languages card info in there

--- src/app/language-card/language-card.component.ts
+++ src/app/language-card/language-card.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-language-card',
   templateUrl: './language-card.component.html',
-  styleUrls: ['./language-card.component.css']
+  styleUrls: ['./language-card.component.css'],
 })
 export class LanguageCardComponent implements OnInit {
+  languageCards: any[] = [
+    {
+      language: 'Urdu',
+      greeting: 'ہیلو دنیا',
+      button: 'Click here to get started!',
+      available: true,
+    },
+    {
+      language: 'French',
+      greeting: 'Bonjour le monde',
+      button: 'Currently not available',
+      available: false,
+    },
+    {
+      language: 'Spanish',
+      greeting: 'Hola Mundo',
+      button: 'Currently not available',
+      available: false,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
