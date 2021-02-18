2765716950623c3afc9db6a69682b03514159267
Farshad Shafii <farshad.shafii@gmail.com>
2021-02-18 16:02:15 +0000

    imported ICategories interface and CATEGORIES object

--- src/app/urdu/urdu.component.ts
+++ src/app/urdu/urdu.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ICategories } from './categories';
+import { CATEGORIES } from './categories-list';
 
 @Component({
   selector: 'app-urdu',
   templateUrl: './urdu.component.html',
-  styleUrls: ['./urdu.component.css']
+  styleUrls: ['./urdu.component.css'],
 })
 export class UrduComponent implements OnInit {
+  categories: ICategories[] = CATEGORIES;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
