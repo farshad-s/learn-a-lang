7b0a2a1c380bc480aec5a4e0b025d9cb14c5ddad
Farshad Shafii <farshad.shafii@gmail.com>
2021-02-18 14:59:19 +0000

    created Urdu Component files

---
+++ src/app/urdu/urdu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-urdu',
+  templateUrl: './urdu.component.html',
+  styleUrls: ['./urdu.component.css']
+})
+export class UrduComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
