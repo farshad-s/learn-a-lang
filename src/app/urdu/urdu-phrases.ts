6a48c7c7c9932a85ddc0dc761407c45477c61881
Farshad Shafii <farshad.shafii@gmail.com>
2021-02-22 16:43:27 +0000

    created variable to hold urdu phrases

---
+++ src/app/urdu/urdu-phrases.ts
@@ -0,0 +1,16 @@
+import { IWords } from '../shared/words';
+
+export const PHRASES: IWords[] = [
+  {
+    word: 'How are you?',
+    translation: 'Kia Haaleh?',
+  },
+  {
+    word: 'What is your name?',
+    translation: 'Aap keh naam Kiya hein?',
+  },
+  {
+    word: 'My name is ___',
+    translation: 'Meri naam ___ hein',
+  },
+];
