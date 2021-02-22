e0a2fdb686b6481171645cada939d1daee9ec7c7
Farshad Shafii <farshad.shafii@gmail.com>
2021-02-22 16:41:09 +0000

    updated description of each category

--- src/app/shared/categories-list.ts
+++ src/app/shared/categories-list.ts
@@ -3,17 +3,17 @@ import { ICategories } from './categories';
 export const CATEGORIES: ICategories[] = [
   {
     category: 'Nouns',
-    description: 'Here is a card full of nouns',
+    description: 'Here is a card with nouns',
     button: 'Click here to get started!',
   },
   {
     category: 'Verbs',
-    description: 'Here is a card full of verbs',
+    description: 'Here is a card with verbs',
     button: 'Click here to get started!',
   },
   {
     category: 'Common Phrases',
-    description: 'Here is a card full of common phrases',
+    description: 'Here is a card with common phrases',
     button: 'Click here to get started!',
   },
 ];
