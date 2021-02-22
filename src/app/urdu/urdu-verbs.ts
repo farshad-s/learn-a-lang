8d48cdcc867b06392d9a427eefe3322bb819b62c
Farshad Shafii <farshad.shafii@gmail.com>
2021-02-22 16:14:43 +0000

    created a variable to hold verbs

---
+++ src/app/urdu/urdu-verbs.ts
@@ -0,0 +1,16 @@
+import { IWords } from '../shared/words';
+
+export const VERBS: IWords[] = [
+  {
+    word: 'I ate',
+    translation: 'Meine Kaya',
+  },
+  {
+    word: 'I went',
+    translation: 'Mein...',
+  },
+  {
+    word: 'I forgot',
+    translation: 'Mein...',
+  },
+];
