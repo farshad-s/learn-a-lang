844e1d4a25b669fd8a587dfaf8237115da475f9b
Farshad Shafii <farshad.shafii@gmail.com>
2021-02-22 13:29:28 +0000

    added code to change the modal content when clicking on the button

--- src/app/urdu/urdu.component.ts
+++ src/app/urdu/urdu.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { ICategories } from '../shared/categories';
 import { CATEGORIES } from '../shared/categories-list';
 import { ModalComponent } from '../shared/modal/modal.component';
+import { IWords } from '../shared/words';
+import { WORDS } from './urdu-words';
 
 @Component({
   selector: 'app-urdu',
@@ -10,6 +12,7 @@ import { ModalComponent } from '../shared/modal/modal.component';
 })
 export class UrduComponent implements OnInit {
   categories: ICategories[] = CATEGORIES;
+  urduWords: IWords[] = WORDS;
 
   constructor() {}
 
@@ -19,5 +22,17 @@ export class UrduComponent implements OnInit {
 
   openModal() {
     this.modal.open();
+    for (let i = 0; i < this.urduWords.length; i++) {
+      this.modal.modal.nativeElement.firstChild.firstChild.innerText +=
+        ' Word: ' +
+        this.urduWords[i].word +
+        ' Translation: ' +
+        this.urduWords[i].translation;
+    }
+    console.log(this.modal);
+    console.log(this.modal.modal);
+    console.log(this.modal.modal.nativeElement);
+    console.log(this.modal.modal.nativeElement.firstChild.firstChild);
+    console.log(this.modal.modal.nativeElement.firstChild.lastChild);
   }
 }
