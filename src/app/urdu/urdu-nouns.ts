afb7ef8b46c1e1c7c1141146bb15e8eb8f1dc07c
Farshad Shafii <farshad.shafii@gmail.com>
2021-02-23 14:04:41 +0000

    changed name from urdu-words to urdu-nouns

---
+++ src/app/urdu/urdu-nouns.ts
@@ -0,0 +1,40 @@
+import { IWords } from '../shared/words';
+
+export const NOUNS: IWords[] = [
+  {
+    word: 'Kabootar',
+    translation: 'Pigeon',
+  },
+  {
+    word: 'Sher',
+    translation: 'Lion',
+  },
+  {
+    word: 'Babar',
+    translation: 'Tiger',
+  },
+  {
+    word: 'Kabootar',
+    translation: 'Pigeon',
+  },
+  {
+    word: 'Sher',
+    translation: 'Lion',
+  },
+  {
+    word: 'Babar',
+    translation: 'Tiger',
+  },
+  {
+    word: 'Kabootar',
+    translation: 'Pigeon',
+  },
+  {
+    word: 'Sher',
+    translation: 'Lion',
+  },
+  {
+    word: 'Babar',
+    translation: 'Tiger',
+  },
+];
