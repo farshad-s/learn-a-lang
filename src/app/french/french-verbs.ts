8daef8d83e4b43bd2e955c6a03366f1b8ddd1ade
Farshad Shafii <farshad.shafii@gmail.com>
2021-02-24 12:20:38 +0000

    added object for french verbs

---
+++ src/app/french/french-verbs.ts
@@ -0,0 +1,16 @@
+import { IWords } from '../shared/words';
+
+export const FRENCHVERBS: IWords[] = [
+  {
+    word: 'I ate',
+    translation: '',
+  },
+  {
+    word: 'I went',
+    translation: '',
+  },
+  {
+    word: 'I forgot',
+    translation: '',
+  },
+];
