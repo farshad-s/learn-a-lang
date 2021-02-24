d7fe23e6675996498ae96f783821425492c6ada7
Farshad Shafii <farshad.shafii@gmail.com>
2021-02-24 11:31:23 +0000

    added phrases object and filled it out partially

---
+++ src/app/french/french-phrases.ts
@@ -0,0 +1,16 @@
+import { IWords } from '../shared/words';
+
+export const FRENCHPHRASES: IWords[] = [
+  {
+    word: 'My name is ___',
+    translation: '',
+  },
+  {
+    word: 'What is your name?',
+    translation: '',
+  },
+  {
+    word: 'How are you?',
+    translation: '',
+  },
+];
