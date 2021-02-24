55a5da56b256f7b474fb66341836c8067b5908af
Farshad Shafii <farshad.shafii@gmail.com>
2021-02-24 10:17:46 +0000

    created file + created object and filled it with information

---
+++ src/app/french/french-nouns.ts
@@ -0,0 +1,40 @@
+import { IWords } from '../shared/words';
+
+export const NOUNS: IWords[] = [
+  {
+    word: 'Pigeon/Pigeonne',
+    translation: 'Pigeon',
+  },
+  {
+    word: 'Lion/Lionne',
+    translation: 'Lion',
+  },
+  {
+    word: 'Tigre/Tigresse',
+    translation: 'Tiger',
+  },
+  {
+    word: 'Chat/Chatte',
+    translation: 'Cat',
+  },
+  {
+    word: 'Âne/Ânesse',
+    translation: 'Donkey',
+  },
+  {
+    word: 'La grenouille',
+    translation: 'Frog',
+  },
+  {
+    word: 'Girafe',
+    translation: 'Giraffe',
+  },
+  {
+    word: 'Poney',
+    translation: 'Pony',
+  },
+  {
+    word: 'Lapin/Lapine',
+    translation: 'Rabbit',
+  },
+];
