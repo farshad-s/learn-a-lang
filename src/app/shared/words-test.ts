b4cabba169a202aea22a25d8f6430d276378f881
Farshad Shafii <farshad.shafii@gmail.com>
2021-02-25 17:32:39 +0000

    created a test file to hold the array with different words

---
+++ src/app/shared/words-test.ts
@@ -0,0 +1,32 @@
+export let words = [
+  {
+    language: 'Urdu',
+    type: 'Noun',
+    word: 'hello',
+    translation: 'hey',
+  },
+  {
+    language: 'Urdu',
+    type: 'Noun',
+    word: 'help',
+    translation: 'me',
+  },
+  {
+    language: 'Urdu',
+    type: 'Verb',
+    word: 'I am eating',
+    translation: 'Meine Khaya',
+  },
+  {
+    language: 'Urdu',
+    type: 'Verb',
+    word: 'I am eating',
+    translation: 'Meine Khaya',
+  },
+  {
+    language: 'Urdu',
+    type: 'Verb',
+    word: 'I am eating',
+    translation: 'Meine Khaya',
+  },
+];
