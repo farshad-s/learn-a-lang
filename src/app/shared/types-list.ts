e4b779aad12334dfb016941623a4d0ec4204b728
Farshad Shafii <farshad.shafii@gmail.com>
2021-03-17 10:54:26 +0000

    created a list of types to be used

---
+++ src/app/shared/types-list.ts
@@ -0,0 +1,13 @@
+import { ITypes } from './types';
+
+export const TYPES: ITypes[] = [
+  {
+    category: 'Animals',
+  },
+  {
+    category: 'Fruits',
+  },
+  {
+    category: 'Vehicles',
+  },
+];
