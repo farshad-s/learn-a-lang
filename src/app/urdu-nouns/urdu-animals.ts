01da97939626d912d8e2a359d535564cc857d94a
Farshad Shafii <farshad.shafii@gmail.com>
2021-03-18 21:24:54 +0000

    created file for urdu animals object

---
+++ src/app/urdu-nouns/urdu-animals.ts
@@ -0,0 +1,40 @@
+import { IWords } from '../shared/words';
+
+export const URDUANIMALS: IWords[] = [
+  {
+    word: 'Pigeon',
+    translation: 'Kabootar',
+  },
+  {
+    word: 'Lion',
+    translation: 'Sher',
+  },
+  {
+    word: 'Tiger',
+    translation: 'Babar',
+  },
+  {
+    word: 'Cat',
+    translation: 'Billi',
+  },
+  {
+    word: 'Donkey',
+    translation: 'Gadha',
+  },
+  {
+    word: 'Frog',
+    translation: 'Maindak',
+  },
+  {
+    word: 'Giraffe',
+    translation: 'Zarafah',
+  },
+  {
+    word: 'Pony',
+    translation: 'Tattu',
+  },
+  {
+    word: 'Rabbit',
+    translation: 'Khargosh',
+  },
+];
