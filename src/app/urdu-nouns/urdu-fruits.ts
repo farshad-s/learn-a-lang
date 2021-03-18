8f4455d0bea79d6b33757dc95d8242b7efc8ece0
Farshad Shafii <farshad.shafii@gmail.com>
2021-03-18 21:53:07 +0000

    created file for urdu fruits object

---
+++ src/app/urdu-nouns/urdu-fruits.ts
@@ -0,0 +1,40 @@
+import { IWords } from '../shared/words';
+
+export const URDUFRUITS: IWords[] = [
+  {
+    word: 'Apple',
+    translation: '',
+  },
+  {
+    word: 'Banana',
+    translation: '',
+  },
+  {
+    word: 'Orange',
+    translation: '',
+  },
+  {
+    word: 'Strawberry',
+    translation: '',
+  },
+  {
+    word: 'Melon',
+    translation: '',
+  },
+  {
+    word: 'Grape',
+    translation: '',
+  },
+  {
+    word: 'Pomegranate',
+    translation: '',
+  },
+  {
+    word: 'Watermelon',
+    translation: '',
+  },
+  {
+    word: 'Pineapple',
+    translation: '',
+  },
+];
