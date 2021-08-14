da466eccdabf8a29272795901dfa7a850331cb9a
Farshad Shafii <farshad.shafii@gmail.com>
2021-08-14 13:37:54 +0100

    urdu-vehicles.ts: add: created file for urdu vehicles object

---
+++ src/app/urdu-nouns/urdu-vehicles.ts
@@ -0,0 +1,36 @@
+import { IWords } from '../shared/words';
+
+export const URDUVEHICLES: IWords[] = [
+  {
+    word: 'Car',
+    translation: '',
+  },
+  {
+    word: 'Motorcycle',
+    translation: '',
+  },
+  {
+    word: 'Van',
+    translation: '',
+  },
+  {
+    word: 'Bicycle',
+    translation: '',
+  },
+  {
+    word: 'Plane',
+    translation: '',
+  },
+  {
+    word: 'Boat',
+    translation: '',
+  },
+  {
+    word: 'Ship',
+    translation: '',
+  },
+  {
+    word: 'Train',
+    translation: '',
+  },
+];
