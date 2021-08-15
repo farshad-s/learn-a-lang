73bd5068345af7bf6854f21e940abcc8aa7a3e2c
Farshad Shafii <farshad.shafii@gmail.com>
2021-08-15 21:40:46 +0100

    nav-bar.component.ts: add: added boolean to check if its homepage or not

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
   pageTitle: string = 'Learn a Language';
+  homepage: boolean = false;
 
   constructor() {}
 
