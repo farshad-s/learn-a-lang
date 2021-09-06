9482821d2f8cc00ffbdd5523b58fbfcdfa1cabe9
Farshad Shafii <farshad.shafii@gmail.com>
2021-09-06 22:38:28 +0100

    verb-types.ts: add: created file + added past, present, future tense to verb types

---
+++ src/app/shared/verb-types.ts
@@ -0,0 +1,13 @@
+import { ITypes } from './types';
+
+export const VERBTYPES: ITypes[] = [
+  {
+    category: 'Past Tense',
+  },
+  {
+    category: 'Present Tense',
+  },
+  {
+    category: 'Future Tense',
+  },
+];
